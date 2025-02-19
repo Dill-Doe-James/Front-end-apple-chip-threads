6d9ae93d3c6483cfd57c9f54ba8105513f41e1e0
Dill-Doe-James <hungb2303818@student.ctu.edu.vn>
2025-02-20 06:49:30 +0700

    Update PhanSo.java

--- PhanSo.java
+++ PhanSo.java
@@ -8,7 +8,7 @@ package bai03;
  *
  * @author LENOVO
  */
-import java.util.Scanner
+import java.util.Scanner;
 
 public class PhanSo {
 
@@ -19,24 +19,29 @@ public class PhanSo {
     public PhanSo() {
 
     }
-
-    public PhanSo(int mau, int tu) {
+    public PhanSo(int tu){
+        this.tu = tu;
+    }
+    public PhanSo(int mau){
+        this.mau = mau;
+    }
+    public PhanSo(int tu, int mau) {
         this.tu = tu;
         this.mau = mau;
     }
     public PhanSo(PhanSo s){
-        this.tu = s.tu;
+        this.tu = new PhanSo(s.tu);
         this.mau = s.mau;
     }
-    void nhap() {
+    public void nhap() {
         Scanner sc = new Scanner(System.in);
         boolean dk = true;
         while (dk) {
             try {
-                System.out.println("Nhap mau so: ");
-                this.mau = sc.nextInt();
                 System.out.println("Nhap tu so: ");
                 this.tu = sc.nextInt();
+                System.out.println("Nhap mau so: ");
+                this.mau = sc.nextInt();
                 if (mau == 0) {
                     System.out.println("Yeu cau nhap lai");
                     dk = true;
@@ -45,20 +50,36 @@ public class PhanSo {
                 }
             } catch (Exception e) {
                 System.out.println("Yeu cau nhap lai");
+                sc.next();
             }
         }
     }
-    void hienthi(){
+    public void hienthi(){
         if(this.tu == 0) System.out.println(0);
-        if(this.mau == 1) System.out.print(tu);
+        if(this.mau == 1) System.out.println(tu);
         else{
-            System.out.print(tu + "/" + mau);
+            System.out.println(tu + "/" + mau);
         }
     }
-    public void UCLN(){
-        
+    public int UCLN(){
+        //ucln 6 va 4 = 2
+        int min = Math.min(this.mau,this.tu);
+        for(int i = min;i>=2;i--){
+            if(this.mau%i==0 && this.tu%i==0)
+                return i;
+        }
+        return 1;
     }
-    
+    public int BCNN(int a, int b){
+        int max = Math.max(a,b);
+        int save = max;
+        for (int i = max; i<=a*b;i++){
+            if(i%a==0 && i%b==0){
+                save = i;
+            }
+        }
+        return save;
+    }
     public void nghichDao(){
         int temp;
         temp = this.mau;
@@ -67,8 +88,9 @@ public class PhanSo {
     }
     public PhanSo giaTriNghichDao(){
         PhanSo n = new PhanSo();
+        int temp = this.tu;
         n.tu = this.mau;
-        n.mau = this.tu;
+        n.mau = temp;
         return n;   
     }
     public float giaTriThuc(){
@@ -79,7 +101,44 @@ public class PhanSo {
         float y = a.tu/a.mau;
         return x < y;
     }
+    public  PhanSo toiGian(){
+        int r = this.UCLN();
+        if(r>0){
+            this.tu /= r;
+            this.mau/= r;
+        }
+        return this;
+    }
     public PhanSo cong(PhanSo a){
-        
+        PhanSo n = new PhanSo(this);
+        int lcm = BCNN(n.mau,a.mau);
+        a.tu = a.tu*(lcm/a.mau);
+        n.tu = n.tu*(lcm/n.mau);
+        n.tu=n.tu+a.tu;
+        n.mau = lcm;
+        n = n.toiGian();
+        return n;
+    }
+    public PhanSo tru(PhanSo a){
+        int lcm = BCNN(this.mau,a.mau);
+        a.tu = a.tu*lcm/a.mau;
+        this.tu = this.tu*lcm/this.mau;
+        a.tu = this.tu-a.tu;
+        a.mau = lcm;
+        a = a.toiGian();
+        return a;
+    }
+    public PhanSo nhan(PhanSo a){
+        a.tu = a.tu*this.tu;
+        a.mau = a.mau*this.mau;
+        a = a.toiGian();
+        return a;
+    }
+    public PhanSo chia(PhanSo a){
+        PhanSo n = a.giaTriNghichDao();
+        n.tu = this.tu*n.tu;
+        n.mau = n.mau*this.mau;
+        n = n.toiGian();
+        return n;
     }
 }
