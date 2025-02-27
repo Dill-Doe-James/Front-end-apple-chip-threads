d40e26ab31b349ca0c96c6aa0204a5585fcdcbca
Dill-Doe-James <hungb2303818@student.ctu.edu.vn>
2025-02-27 09:39:33 +0700

    Delete quanlySinhVien directory

--- quanlySinhVien/Manager.java
+++
@@ -1,9 +0,0 @@
-package quanlySinhVien;
-
-public class Manager {
-    public static void main(String[] args){
-        SinhVien sv = new SinhVien();
-        sv.nhap();
-        sv.xuat();
-    }
-}
